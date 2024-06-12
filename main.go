64c950a131e5df780aee7d6f2d5847dcc07257c9
rjniemela <rjniem@gmail.com>
2024-06-12 23:35:40 +0300

    Pre routing

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsPrint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	status := fmt.Sprintf("Hits: %v", cfg.fileserverHits)
+	w.Write([]byte(status))
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
-
+	apiCfg := apiConfig{fileserverHits: 0}
+	handler := http.FileServer(http.Dir(filepathRoot))
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /metrics", apiCfg.middlewareMetricsPrint)
+	mux.HandleFunc("GET /healthz", handleHealthz)
+	mux.HandleFunc("/reset", apiCfg.middlewareMetricsReset)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
